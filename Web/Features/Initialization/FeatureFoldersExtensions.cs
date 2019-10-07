b30519bf5f6bf83a8b334a03b6564f570f7aab72
Bjarte Aune Olsen <bjarte.olsen@norgesgruppen.no>
2019-10-07 09:50:42 +0200

    Added services and routing for views and controllers in feature folders

---
+++ Web/Features/Initialization/FeatureFoldersExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Web.Features.Initialization
+{
+    public static class FeatureFoldersExtensions
+    {
+        public static IMvcBuilder AddFeatureFolders(this IMvcBuilder builder)
+        {
+            builder
+                .AddRazorOptions(options =>
+                    {
+                        options.ViewLocationFormats.Add("/Features/{1}/{0}.cshtml");
+                        options.ViewLocationFormats.Add("/Features/{1}/Views/{0}.cshtml");
+                        options.ViewLocationFormats.Add("/Features/Shared/{0}.cshtml");
+                        options.ViewLocationFormats.Add("/Features/Shared/Views/{0}.cshtml");
+                    }
+                );
+
+            return builder;
+        }
+    }
+}
