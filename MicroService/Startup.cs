9b4ac6a0b492f794a72f0e392d49d2f0eb796302
Bjarte Aune Olsen <bjarte.olsen@norgesgruppen.no>
2019-10-28 10:23:35 +0100

    Microservice: Added Swagger for API documentation

--- MicroService/Startup.cs
+++ MicroService/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace Microservice
 {
@@ -18,6 +19,11 @@ namespace Microservice
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Demo API", Version = "v1" });
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -26,6 +32,14 @@ namespace Microservice
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Demo API V1");
+                c.RoutePrefix = string.Empty;
+            });
 
             app.UseRouting();
 
